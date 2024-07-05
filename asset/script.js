e12974276efa169e4e82897c8202fe72bed35f04
fajryalvin12 <fajryalvin12@gmail.com>
2024-07-05 08:22:24 +0700

    add login.html and event.html page, then refactor whole part of footer section

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,25 @@
+// footer-home
+    const footer1 = document.getElementById("footer-1")
+    const footer2 = document.getElementById("footer-2")
+    const footer3 = document.getElementById("footer-3")
+
+    function addContent (targetElement, content){
+        content.forEach(item => {
+            const textContainer = document.createElement("li")
+            textContainer.textContent = item 
+            targetElement.appendChild(textContainer)
+        })
+    }
+
+const content = [
+    ["About us", "Features", "Blog", "Payment", "Mobile Apps"],
+    ["Booking", "Create Event", "Discover", "Register"],
+    ["Partnership", "Help", "Terms of Service","Privacy Policy", "Sitemap"],
+]
+
+addContent(footer1, content[0])
+addContent(footer2, content[1])
+addContent(footer3, content[2])
 // navigation-bar-home
 
 const menuButton = document.getElementById("menu")
@@ -21,28 +43,6 @@ const menuButton = document.getElementById("menu")
 
         }
 
-// footer-home
-    const footer1 = document.getElementById("footer-1")
-    const footer2 = document.getElementById("footer-2")
-    const footer3 = document.getElementById("footer-3")
-
-    function addContent (targetElement, content){
-        content.forEach(item => {
-            const textContainer = document.createElement("li")
-            textContainer.textContent = item 
-            targetElement.appendChild(textContainer)
-        })
-    }
-
-const content = [
-    ["About us", "Features", "Blog", "Payment", "Mobile Apps"],
-    ["Booking", "Create Event", "Discover", "Register"],
-    ["Partnership", "Help", "Terms of Service","Privacy Policy", "Sitemap"],
-]
-
-addContent(footer1, content[0])
-addContent(footer2, content[1])
-addContent(footer3, content[2])
 
 // eventlist-home
 
